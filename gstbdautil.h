347c4a72c7ec3e5e6b1cc456e8ece9d1d4be7cc9
Raimo Järvi <raimo.jarvi@gmail.com>
2015-03-31 21:58:56 +0300

    Add comments.

--- gstbdautil.h
+++ gstbdautil.h
@@ -27,28 +27,70 @@
 #include <qedit.h>
 #include "gstbdasrc.h"
 
+/**
+ * Returns a description for the specified BDA / DirectShow error code.
+ */
 std::string bda_err_to_str (HRESULT hr);
 
+/**
+ * Returns a friendly name for the specified BDA tuner device.
+ */
 std::string bda_get_tuner_name (IMoniker * tuner_moniker);
 
+/**
+ * Determines input device type from tuner's capabilities. FIXME: One device
+ * can probably support multiple input types (e.g. DVB-T and DVB-C), we don't
+ * currently support that.
+ */
 GstBdaInputType gst_bdasrc_get_input_type (GstBdaSrc * src);
 
+/**
+ * Maps our input type to BDA network type.
+ */
 BOOL gst_bdasrc_get_network_type (GstBdaInputType input_type,
     CLSID & network_type);
 
+/**
+ * Returns a BDA tuning space according to input device type. If a matching
+ * tuning space is found in system tuning spaces, it is returned. Otherwise
+ * a new one is created. FIXME: Use a more generic interface than
+ * IDVBTuningSpace.
+ * @return TRUE if tuning space was found or created successfully
+ */
 BOOL gst_bdasrc_create_tuning_space (GstBdaSrc * src,
     IDVBTuningSpacePtr & tuning_space);
 
+/**
+ * Initializes a tune request according to input device type. FIXME: Use a more
+ * generic interface than IDVBTuneRequest.
+ * @return TRUE if tune request was initialized successfully
+ */
 BOOL gst_bdasrc_tune_request (GstBdaSrc * src,
     IDVBTuneRequestPtr & tune_request);
 
+/**
+ * Connects the specified DirectShow filters. Enumerates filter pins and
+ * tries to connect any unconnected pins (upstream output pin -> downstream
+ * input pin).
+ * @return S_OK if filters were connected successfully, otherwise an error
+ */
 HRESULT gst_bdasrc_connect_filters (GstBdaSrc * src,
     IBaseFilter * filter_upstream, IBaseFilter * filter_downstream);
 
+/**
+ * Creates a filter with the specified CLSID and connects it to the specified
+ * upstream filter.
+ * @return S_OK if filter was created successfully, otherwise an error
+ */
 HRESULT gst_bdasrc_load_filter (GstBdaSrc * src, ICreateDevEnum * sys_dev_enum,
     REFCLSID clsid, IBaseFilter * upstream_filter,
     IBaseFilter ** downstream_filter);
 
+/**
+ * Creates a transport stream capture filter and connects it to our
+ * GstBdaGrabber.
+ * @return TRUE if capture filter was created successfully
+ */
 BOOL gst_bdasrc_create_ts_capture (GstBdaSrc * bda_src,
     ICreateDevEnum * sys_dev_enum, IBaseFilterPtr & ts_capture);
 
